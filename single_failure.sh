374368b2a0ffecdc99484cbd10278444a4c183d8
Tran Ly Vu <vutransingapore@gmail.com>
2018-12-11 11:50:16 +0800

    Add test scripts for different scenarios
    Signed-off-by: Tran Ly Vu <tranly.vu@accellion.com>

---
+++ single_failure.sh
@@ -0,0 +1,67 @@
+
+function contains () {
+  	local e
+  	for e in "${@:2}"
+	do 
+		if [ "$e" == "$1" ]; then 
+			echo 1
+			return 1;
+		fi
+	done
+  	echo 0
+}
+
+verbose=$(contains "-v" "$@")
+grade=0
+
+echo "============================================"
+echo "Grading Started"
+echo "============================================"
+echo "Single Failure Scenario"
+echo "============================"
+if [ $verbose -eq 0 ]; then
+	make clean > /dev/null
+	make > /dev/null
+	./Application testcases/singlefailure.conf > /dev/null
+else
+	make clean 
+	make 
+	./Application testcases/singlefailure.conf 
+fi
+joincount=`grep joined dbg.log | cut -d" " -f2,4-7 | sort -u | wc -l`
+if [ $joincount -eq 100 ]; then
+	grade=`expr $grade + 10`
+	echo "Checking Join..................10/10"
+else
+	joinfrom=`grep joined dbg.log | cut -d" " -f2 | sort -u`
+	cnt=0
+	for i in $joinfrom
+	do
+		jointo=`grep joined dbg.log | grep '^ '$i | cut -d" " -f4-7 | grep -v $i | sort -u | wc -l`
+		if [ $jointo -eq 9 ]; then
+			cnt=`expr $cnt + 1`
+		fi
+	done
+	if [ $cnt -eq 10 ]; then
+		grade=`expr $grade + 10`
+		echo "Checking Join..................10/10"
+	else
+		echo "Checking Join..................0/10"
+	fi
+fi
+failednode=`grep "Node failed at time" dbg.log | sort -u | awk '{print $1}'`
+failcount=`grep removed dbg.log | sort -u | grep $failednode | wc -l`
+if [ $failcount -ge 9 ]; then
+	grade=`expr $grade + 10`
+	echo "Checking Completeness..........10/10"
+else
+	echo "Checking Completeness..........0/10"
+fi
+failednode=`grep "Node failed at time" dbg.log | sort -u | awk '{print $1}'`
+accuracycount=`grep removed dbg.log | sort -u | grep -v $failednode | wc -l`
+if [ $accuracycount -eq 0 ] && [ $failcount -gt 0 ]; then
+	grade=`expr $grade + 10`
+	echo "Checking Accuracy..............10/10"
+else
+	echo "Checking Accuracy..............0/10"
+fi
